1b9409a097d4f18f6bf8d000203eb590b73fc960
Yanick Champoux <yanick@babyl.dyndns.org>
2011-01-23 15:31:48 -0500

    be explicit with the fields

--- schema/lib/cpanvote/Schema/Result/Sessions.pm
+++ schema/lib/cpanvote/Schema/Result/Sessions.pm
@@ -4,7 +4,23 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('Sessions');
-__PACKAGE__->add_columns(qw/id session_data expires/);
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'varchar',
+        size => 72,
+        is_nullable => 0,
+    },
+    session_data => {
+        data_type => 'text',
+        is_nullable => 1,
+    },
+    expires => {
+        data_type => 'integer',
+        is_nullable => 1,
+    },
+);
+
 __PACKAGE__->set_primary_key('id');
 
 1;
